1e73b50ab32dd90f715c241308eea01c48f2e144
MC BTay <mcbtay@gmail.com>
2014-09-26 18:11:07 -0400

    Moving FAQ/Preview/Register piece on home log in screen.  Also hooking up Preview link to the twitter link given to me by Derrick.

--- views/templates/footer-nav-login.php
+++ views/templates/footer-nav-login.php
@@ -1,5 +1,5 @@
 
-				<div class="login-navigation">
+				<!--div class="login-navigation">
 					<a href="<?php echo base_url(); ?>account/faq">FAQ</a>
 					&nbsp;|&nbsp;
 					<a href="<?php echo base_url(); ?>account/preview">Preview</a>
@@ -9,7 +9,7 @@
 					<?php else: ?>
 						<a href="<?php echo base_url(); ?>account/login">Login</a>
 					<?php endif; ?>
-				</div>
+				</div-->
 				<div id="footer-login">
 					<a href="mailto:info@babesforbitcoin.com">Contact Us</a>
 					&nbsp;|&nbsp;
