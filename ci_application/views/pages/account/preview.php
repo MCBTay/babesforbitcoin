d731f0c8f49f04c905b51c12805528468010e19b
MC BTay <mcbtay@gmail.com>
2014-09-28 13:38:16 -0400

    first iteration of the preview page, no models yet

--- ci_application/views/pages/account/preview.php
+++ ci_application/views/pages/account/preview.php
@@ -4,9 +4,11 @@
 		<div class="logo-large">
 			<h1><img alt="Babes for Bitcoin" src="<?php echo base_url(); ?>assets/img/babes-for-bitcoin-logo-large.png"></h1>
 		</div>
-		<div class="faq-copy copy">
-			<h2>Preview</h2>
-			<p>There will be no preview available until our &#579;eta has ended.</p>
-		</div>
+        <div id="preview-banner">
+            <p>Newest Models</p>
+        </div>
+        <div id="preview-banner">
+            <p>Sign up is free, click to join!</p>
+        </div>
 	</div>
 </div>
